a5a9a74bd6febcab96593dd9f832c9f07ba8b433
Collin <collinmpage@gmail.com>
2021-01-20 12:03:43 -0500

    Finished pybank assignment

--- PyBank/Solved/main.py
+++ PyBank/Solved/main.py
@@ -22,18 +22,53 @@ with open(budget_csv) as csvfile:
     revenue_change = []
     date = []
 
+#calculating the total months and revenue
     for row in csv_reader:
 
         total_months = total_months + 1
-        date.append(row[1])
+        date.append(row[0])
 
         total_revenue = total_revenue + int(row[1])
         revenue.append(row[1])
 
 
-
+#printing the first part of the assignment
 
 print("Financial Analysis")
 print("---------------------")
 print("Total Months: " + str(total_months))
-print("Total Revenue: " + str(total_revenue))
+print("Total Revenue: $" + str(total_revenue))
+
+#finding the average, max, and minimum changes
+
+for i in range(len(revenue)-1):
+    change = int(revenue[i+1]) - int(revenue[i])
+    revenue_change.append(change)
+
+avg_revenue_change = round(sum(revenue_change)/len(revenue_change), 2)
+    
+max_revenue_change = round(max(revenue_change),2)
+max_revenue_change_date = str(date[revenue_change.index(max(revenue_change)) +1])
+    
+min_revenue_change = round(min(revenue_change),2)
+min_revenue_change_date = str(date[revenue_change.index(min(revenue_change)) +1])
+
+#printing the remainder of the assignment
+
+print("Average Revenue Change: $" + str(avg_revenue_change))
+print("Greatest Increase in Profits: " + str(max_revenue_change_date) + "($" + str(max_revenue_change) +")")
+print("Greatest Decrease in Profits: " + str(min_revenue_change_date) + "($" + str(min_revenue_change) +")")
+
+
+#send to txt file
+
+with open(file_output, "w") as txt_file:
+    txt_file.write("Total Months: " + str(total_months))
+    txt_file.write("\n")
+    txt_file.write("Total Revenue: " + "$" + str(total_revenue))
+    txt_file.write("\n")
+    txt_file.write("Average Change: " + "$" + str(avg_revenue_change))
+    txt_file.write("\n")
+    txt_file.write("Greatest Increase: " + str(max_revenue_change_date)+ " ($" + str(max_revenue_change) +")") 
+    txt_file.write("\n")
+    txt_file.write("Greatest Decrease: " + str(min_revenue_change_date)+ " ($" + str(min_revenue_change)+")")
